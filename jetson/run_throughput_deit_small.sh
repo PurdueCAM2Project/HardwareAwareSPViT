6f2d57af5d167bf514570d81df3acb902a7bd153
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2023-09-11 00:17:43 -0400

    Added --no-distributed to jetson deit small script

--- jetson/run_throughput_deit_small.sh
+++ jetson/run_throughput_deit_small.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python SPViT_DeiT/main_pruning.py --data-path /usr/local/data/ImageNet1K/ILSVRC/Data/CLS-LOC/  --config config/spvit_deit_sm_l30_t32_ft.json --resume bin/spvit_deit_sm_l30_t32_dist.pth --throughput 
+python SPViT_DeiT/main_pruning.py --no-distributed --data-path /usr/local/data/ImageNet1K/ILSVRC/Data/CLS-LOC/  --config config/spvit_deit_sm_l30_t32_ft.json --resume bin/spvit_deit_sm_l30_t32_dist.pth --throughput 
