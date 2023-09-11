ac262476ff9db18ffe4c980abf902d2ee1b5b4e1
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2023-09-11 00:17:16 -0400

    Not using distributed evaluation as it does not seem to be supported on PyTorch build of TX2. Added commandline option --no-distributed

--- jetson/run_throughput_deit_small.sh
+++ jetson/run_throughput_deit_small.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=3146 --use_env main_pruning.py --config config/spvit_deit_sm_l30_t32_ft.json --resume bin/spvit_deit_sm_l30_t32_dist.pth --throughput 
+python SPViT_DeiT/main_pruning.py --data-path /usr/local/data/ImageNet1K/ILSVRC/Data/CLS-LOC/  --config config/spvit_deit_sm_l30_t32_ft.json --resume bin/spvit_deit_sm_l30_t32_dist.pth --throughput 
